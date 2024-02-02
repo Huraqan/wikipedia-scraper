c646ecefc5c4b0d40d857094fe91341dc6f63085
unknown <indesteege.sebastiaan@gmail.com>
2024-02-02 10:32:48 +0100

    first code commit

---
+++ main.py
@@ -0,0 +1,52 @@
+################################
+### venv-ws\Scripts\activate ###
+### personal reminder...     ###
+################################
+
+from requests import Session
+
+from src.scraper import WikipediaScraper
+
+
+def pick_countries(scraper) -> list:
+    countries = scraper.get_countries()
+    
+    while True:
+        answer = input(
+            f"\nPick one or more countries from {countries}"
+            + " separated by a comma or pick 'all': "
+        )
+        
+        if answer == "all":
+            return countries
+        
+        answer_list = answer.replace(" ", "").split(",")
+        answer_list = [answer for answer in answer_list if answer in countries]
+        
+        if len(answer_list) > 0:
+            return answer_list
+        else:
+            print("\nWrong input. Sheeeeeesh... Two letters per country, separate by a comma.\n")
+
+
+if __name__ == "__main__":
+    print(
+        "\n=================================\n" +
+        "||  POWER-DUDES Wiki-Scraper   ||" +
+        "\n=================================\n"
+    )
+    scraper = WikipediaScraper(Session())
+    
+    for country in pick_countries(scraper):
+        scraper.get_leaders(country)
+    
+    for (country, country_leaders) in scraper.leaders_data.items():
+        print("\n    country:", country)#, "country_leaders:", type(country_leaders), country_leaders)
+        
+        for i, leader in enumerate(country_leaders):
+            first_paragraph = scraper.get_first_paragraph(leader["wikipedia_url"], country, i)
+            
+            print(f"\n{leader["first_name"]}: {scraper.clean_text(first_paragraph)}\n")
+    
+    scraper.to_json_file("output_json.json")
+    scraper.to_csv_file("output_csv.csv")
---
+++ src/scraper.py
@@ -0,0 +1,191 @@
+# import csv
+import json
+from re import compile
+
+from pandas import DataFrame
+from bs4 import BeautifulSoup
+from requests import Session
+
+class WikipediaScraper:
+    """
+    WikipediaScraper class for scraping the first paragraph of country leaders biographies.
+    ...
+    """
+
+    def __init__(self, session: Session):
+        self.base_url: str = "https://country-leaders.onrender.com"
+        self.country_endpoint: str = "/countries"
+        self.leaders_endpoint: str = "/leaders"
+        self.cookies_endpoint: str = "/cookie"
+        self.check_endpoint: str = "/check"
+        self.leaders_data: dict = {}
+        self.session: Session = session
+        self.cookie: object = self.request_cookie()
+
+    def refresh_cookie(self) -> object:
+        """Checks the API cookie status, returning the existing or new cookie object."""
+        
+        print("\nChecking if cookie is still valid...")
+
+        check_request = self.session.get(
+            self.base_url + self.check_endpoint, cookies=self.cookie
+        )
+
+        match check_request.status_code:
+            case 200:
+                print("\nCookie still valid.")
+                return self.cookie
+            case 422:
+                print("\nCookie stale! Refreshing cookie...")
+            case _:
+                print(f"\nStatus: {check_request.status_code}")
+
+        return self.request_cookie()
+
+    def request_cookie(self) -> object:
+        """Requests the API cookie and returns the new cookie object."""
+        print("\nRequesting a new cookie...")
+        
+        cookie_request = self.session.get(self.base_url + self.cookies_endpoint)
+
+        match cookie_request.status_code:
+            case 200:
+                print("Cookie request status: OK")
+            case 400:
+                print("Cookie request status: BAD REQUEST")
+            case 403:
+                print("Cookie request status: FORBIDDEN")
+            case 404:
+                print("Cookie request status: PAGE NOT FOUND")
+
+        if not cookie_request.status_code == 200:
+            return None
+
+        self.cookie = cookie_request.cookies
+
+        return self.cookie
+
+    def get_countries(self) -> list:
+        """Retrieve a list of supported countries from the API."""
+        # request countries and return list
+        countries_request = self.session.get(
+            self.base_url + self.country_endpoint,
+            cookies=self.cookie
+        )
+        return countries_request.json()
+
+    def get_leaders(self, country: str) -> None:
+        """
+        Retrieve and populate leaders_data with the leaders of a specific country.
+
+        - country (str): The name of the country for which leaders are retrieved.
+        """
+        print(f"\nRequesting leaders from {country.upper()}...")
+        
+        # request the data and store as dict in leaders_data ... DICT!!!!!
+        req_leaders = self.session.get(
+            self.base_url + self.leaders_endpoint,
+            params={"country": country},
+            cookies=self.refresh_cookie(),
+        )
+
+        country_leaders = req_leaders.json()
+        self.leaders_data[country] = country_leaders
+        # self.leaders_data = {i: value for i, value in enumerate(req_leaders.json())}
+        # {key:value for (key,value) in dictonary.items()}
+        
+        # print("get leaders:", self.leaders_data)
+
+    def get_first_paragraph(self, wikipedia_url:str, country:str, i:int) -> str:
+        """
+        Retrieve the first paragraph from a Wikipedia URL.
+
+        - wikipedia_url (str): The URL of the Wikipedia page.
+
+        Returns:
+            str: The text of the first paragraph.
+        """
+        print(f"\n{wikipedia_url}\n")
+
+        soup = BeautifulSoup(self.session.get(wikipedia_url).text, "html.parser")
+        
+        content_div = soup.find(name="div", attrs={"class": "mw-content-ltr mw-parser-output"})
+        
+        if content_div is None:
+            print("\nCan't find ltr content div, searching for rtl content...")
+            content_div = soup.find(name="div", attrs={"class": "mw-content-rtl mw-parser-output"})
+        if content_div is None:
+            print("\nCan't find rtl content, continuing with full page...")
+            content_div = soup
+        
+        for tag in content_div.find_all(name="div"):
+            tag.decompose()
+
+        for tag in content_div.find_all(name="p"):
+            if not tag.find(name="b"):
+                continue
+
+            for sup_tag in tag.find_all(name="sup"):
+                print("- Decomposing <sup> tag:", sup_tag.text)
+                sup_tag.decompose()
+
+            self.leaders_data[country][i]["paragraph"] = tag.text
+
+            return tag.text
+
+        return "\n\n    !!! NOTING FOUND !!!\n\n"
+
+    @staticmethod
+    def clean_text(text: str) -> str:
+        compiled_brackets = compile(r"\[[^\]]*\]")
+        compiled_parentheses = compile(r"\(/[^\)]+\)")
+        compiled_commas = compile(r" ?,( *,)*")
+        compiled_spaces = compile(r"  +")
+
+        text_length = len(text)
+
+        text = compiled_brackets.sub("", text)
+        text = compiled_parentheses.sub("", text)
+        text = compiled_spaces.sub(" ", text)
+        text = compiled_commas.sub(",", text)
+
+        if text_length > len(text):
+            print(f"- Regex shortened text by: {text_length - len(text)} characters.")
+        else:
+            print("- Regex had nothing to clean up...")
+
+        return text
+
+    def to_json_file(self, filepath : str):
+        """
+        Store the data structure into a JSON file.
+
+        - filepath (str): The path to the JSON file.
+        """
+        with open(filepath, "w") as file:
+            json.dump(self.leaders_data, file)
+
+    def to_csv_file(self, filepath : str):
+        # DataFrame(self.leaders_data).to_csv(filepath)
+        
+        # field_names = ["Country", "Lea    der", "First Paragraph"]
+        leaders_summary : list = []
+        
+        for (country, leaders) in self.leaders_data.items():
+            # print("country:", country, "leaders;:", leaders)
+            
+            for leader in leaders:
+                leaders_summary.append({
+                    "Country": country,
+                    "Leader": f"{leader["first_name"]} {leader["last_name"]}",
+                    "First Paragraph": leader["paragraph"]
+                    }
+                )
+        
+        df = DataFrame.from_dict(leaders_summary)
+        df.to_csv (filepath, index=False, header=True)
+        
+        # with open(filepath, "w", encoding='Windows-1252') as csvfile:
+        #     writer = csv.DictWriter(csvfile, fieldnames=field_names)
+        #     writer.writeheader()
+        #     writer.writerows(leaders_summary)
